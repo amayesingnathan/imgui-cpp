4b832c6b130d6fe4919a08a5876d95a8d6d01291
amayesingnathan <amayesingnathan123@gmail.com>
2023-02-02 11:05:28 +0000

    Initial commit

---
+++ imgui-cpp/premake5.lua
@@ -0,0 +1,56 @@
+project "imgui-cpp"
+    language "C++"
+    cppdialect "C++20"
+		
+    targetdir 	("%{wks.location}/bin/%{prj.name}/" .. outputDir)
+    objdir 		("%{wks.location}/obj/%{prj.name}/" .. outputDir)
+
+    files 
+    { 
+        "src/**.h", 
+        "src/**.cpp",
+        "src/**.hxx", 
+        "src/**.ixx",
+        "src/**.ixxp",
+    }
+	
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+    includedirs
+    {
+        "%{IncludeDir.imguicpp}",
+    }
+
+	links
+	{
+	}
+	
+    filter { "files:**.hxx" }
+        compileas "HeaderUnit"
+    
+    filter { "files:**.ixx" }
+        compileas "Module"
+        
+    filter { "files:**.ixxp" }
+        compileas "ModulePartition"
+		
+    filter "system:windows"
+        kind "StaticLib"
+        staticruntime "off"
+        systemversion "latest"
+		
+	filter "system:linux"
+        kind "SharedLib"
+        staticruntime "off"
+        pic "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+        symbols "on"
+    filter "configurations:Release"
+		runtime "Release"
+        optimize "on"
