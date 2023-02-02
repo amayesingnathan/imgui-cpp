575aedf758ceb9990c443d339adbe657695b022b
amayesingnathan <amayesingnathan123@gmail.com>
2023-02-02 13:43:37 +0000

    Reorganised repo structure

--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,63 @@ outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
 IncludeDir["imguicpp"] 	= "%{wks.location}/imgui-cpp/src"
-IncludeDir["imgui"] 	= "%{wks.location}/imgui-cpp/dependencies/imgui"
+IncludeDir["imgui"] 	= "%{wks.location}/dependencies/imgui"
 
-include "imgui-cpp"
+
+project "imgui-cpp"
+    language "C++"
+    cppdialect "C++20"
+		
+    targetdir 	("%{wks.location}/bin/%{prj.name}/" .. outputDir)
+    objdir 		("%{wks.location}/obj/%{prj.name}/" .. outputDir)
+
+    files 
+    { 
+        "src/**.h", 
+        "src/**.cpp",
+        "src/**.hxx", 
+        "src/**.ixx",
+        "src/**.ixxp",
+    }
+	
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+    includedirs
+    {
+        "%{IncludeDir.imguicpp}",
+        "%{IncludeDir.imgui}",
+    }
+
+	links
+	{
+	}
+	
+    filter { "files:**.hxx" }
+        compileas "HeaderUnit"
+    
+    filter { "files:**.ixx" }
+        compileas "Module"
+        
+    filter { "files:**.ixxp" }
+        compileas "ModulePartition"
+		
+    filter "system:windows"
+        kind "StaticLib"
+        staticruntime "off"
+        systemversion "latest"
+		
+	filter "system:linux"
+        kind "SharedLib"
+        staticruntime "off"
+        pic "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+        symbols "on"
+    filter "configurations:Release"
+		runtime "Release"
+        optimize "on"
